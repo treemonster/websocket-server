0096d6be3d12ce559d5912e615f4df489b9ab451
chen wen'gao <1696292264@qq.com>
2015-02-16 14:21:01 +0800

    Update server.js

--- demo/server.js
+++ demo/server.js
@@ -1,4 +1,4 @@
-require('../SimpleWebsocket').create
+require('SimpleWebsocket').create
 ({
   port:8100,
   timeout:{keepAlive:5000,firstShake:5000},
@@ -6,17 +6,27 @@ require('../SimpleWebsocket').create
     console.log(text);
     this.write('OK,'+text.length+' charactors received, `'+text+'`');
   },
+  onBinaryOK:function(data){
+    console.log(data);
+    this.write('OK,'+data.length+' bytes received, `'+data+'`');
+  },
+  onTextHeader:function(text){
+    console.log(text);
+  },
   onClose:function(reason,code){
     console.log(this.id);
   },
   debug:true,
+  allowBinary:true,
   enableRule:{
     text:{
+      requireHead:true,
+      joinMessage:true
+    },
+    binary:{
       requireHead:true,
       joinMessage:true
     }
   }
 });
-
-
 
