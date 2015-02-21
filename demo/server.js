833a78e8f3dd1334b422870ae8ea0c791410647f
chen wen'gao <1696292264@qq.com>
2015-02-21 20:12:07 +0800

    Update server.js

--- demo/server.js
+++ demo/server.js
@@ -1,17 +1,33 @@
-var sendData=function(data,socket,keepString){
-  data=data||{};
-  socket.write(function(head){
+var sendData=function(data,client,replyKeep){
+  var msg=[];
+  var socket=client.socket;
+  client.keepList=client.keepList||{};
+  socket.write=socket.write||socket.send;
+  data.header=data.header||{};
+  if(!data.json && !data.text){
+    data.header.nodata=true;
+  }
+  msg.push(function(head){
     var heads=[];
     for(var what in head)
-      heads.push('--'+what.charAt(0)+' '+head[what]);
-    if(keepString!==undefined)heads.push('--r '+keepString);
+      heads.push('--'+what.charAt(0)+(head[what]===true?'':' '+head[what]));
+    if(head.keep){
+      client.keepList[head.keep]=data;
+    }
+    if(replyKeep!==undefined){
+      heads.push('--r '+replyKeep);
+      delete client.keepList[replyKeep];
+    }
     return heads.join('\n');
-  }(data.header=data.header||{nodata:''}));
-  if(!data.header.boundary)return;
-  if(data.json||data.text){
-    socket.write(data.text||JSON.stringify(data.json));
-    socket.write(data.header.boundary+'--');
+  }(data.header));
+  if(data.header.nodata!==true){
+    if(!data.header.boundary)throw new error('错误的格式');
+    if(data.json||data.text){
+      msg.push(data.text||JSON.stringify(data.json));
+      msg.push(data.header.boundary+'--');
+    }
   }
+  while(msg.length)socket.write(msg.shift());
 };
 
 require('SimpleWebsocket').create
@@ -22,19 +38,17 @@ require('SimpleWebsocket').create
     console.log(text);
     sendData({
       header:{
-        msg:'OK,'+text.length+' charactors received, `'+text+'`',
-        nodata:''
+        msg:'OK,'+text.length+' charactors received, `'+text+'`'
       }
-    },this.socket,head.keep||undefined);
+    },this,head.keep);
   },
   onBinaryOK:function(data,head){
     console.log(data);
     sendData({
       header:{
-        msg:'OK,'+data.length+' bytes received, `'+data+'`',
-        nodata:''
+        msg:'OK,'+data.length+' bytes received, `'+data+'`'
       }
-    },this.socket,head.keep||undefined);
+    },this,head.keep);
   },
   onHeader:function(text){
     console.log(text);
